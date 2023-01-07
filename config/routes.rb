7d35f42a3ee38210ba8624ab2fae94d558c63d16
EC2 Default User <youhei3735@gmail.com>
2023-01-07 05:36:58 +0000

    [finish]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-resources :users, only: [:show, :edit]
-root 'homes#top'
-devise_for :user
-get "homes/about" => "homes#about", as: "about"
-resources :post_images, only: [:new, :create, :index, :show, :destroy]
-end
+  devise_for :users
+  root 'homes#top'
+  get 'about'=>'homes#about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
+  end
